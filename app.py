379c96f953228a4762adca9e05b1968745ab92e1
Kate <kate.burovova@gmail.com>
2024-07-17 18:50:24 +0300

    correct text

--- app.py
+++ app.py
@@ -1,61 +1,16 @@
-import os
 import streamlit as st
-import time
-
-import plotly.express as px
-import streamlit.components.v1 as components
-
-from langchain import hub
-from datetime import datetime
-from langchain import callbacks
-from elasticsearch import Elasticsearch
-from elasticsearch import BadRequestError
-from elasticsearch.exceptions import NotFoundError
-from angle_emb import AnglE, Prompts
-from langchain_openai import ChatOpenAI
 from authentificate import check_password
-from utils import (display_distribution_charts,populate_default_values, project_indexes,
-                   populate_terms,create_must_term, create_dataframe_from_response,flat_index_list,
-                   search_elastic_below_threshold)
-#
-#
-# # Init Langchain and Langsmith services
-# os.environ["LANGCHAIN_TRACING_V2"] = "true"
-# os.environ["LANGCHAIN_PROJECT"] = f"streamlit_app : issues_finetuning : development"
-# os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
-# os.environ["LANGCHAIN_API_KEY"] = st.secrets['ld_rag']['LANGCHAIN_API_KEY']
-# os.environ["LANGSMITH_ACC"] = st.secrets['ld_rag']['LANGSMITH_ACC']
-
-
-# # Init openai model
-# OPENAI_API_KEY = st.secrets['ld_rag']['OPENAI_KEY_ORG']
-# llm_chat = ChatOpenAI(temperature=0.0, openai_api_key=OPENAI_API_KEY,
-#              model_name='gpt-4-1106-preview')
-
-es_config = {
-    'host': st.secrets['ld_rag']['ELASTIC_HOST'],
-    'port': st.secrets['ld_rag']['ELASTIC_PORT'],
-    'api_key': st.secrets['ld_rag']['ELASTIC_API']
-}
+from utils import (project_indexes,flat_index_list)
 
 ########## APP start ###########
 st.set_page_config(layout="wide")
-
-# description
-st.markdown('App relies on data, collected and enriched by our team and provides citations for all sources used for '
-            'answers. \n'
-            'If you are running this app from a mobile device, tap on any '
-            'empty space to apply changes to input fields. '
-            'If you experience any technical issues, please [submit the form](https://docs.google.com/forms/d/e/1FAIpQLSfZTr4YoXXsjOOIAMVGYCeGgXd6LOsCQusctJ7hZODaW5HzGQ/viewform?pli=1) by selecting "LD app Technical Issue" for '
-            'the type of request. To give feedback for request output, '
-            'please use the feedback form at the end of the page.')
 
 # Authorise user
 if not check_password():
     st.stop()
 
 # Get input parameters
-st.markdown('### Please select search parameters ')
+st.markdown('### Please select testing parameters ')
 
 selected_index = None
 search_option = st.radio(
@@ -91,9 +46,9 @@ st.write("You've selected", prompt)
 
 selected_start_date = st.date_input("Select start date for the timeframe you'd like to test:")
 formatted_start_date = selected_start_date.strftime("%Y-%m-%d")
-st.write("You selected start date:", selected_start_date)
+st.write("You've selected start date:", selected_start_date)
 selected_end_date = st.date_input("Select end date for the timeframe you'd like to test:")
 formatted_end_date = selected_end_date.strftime("%Y-%m-%d")
-st.write("You selected end date:", selected_start_date)
+st.write("You've selected end date:", selected_start_date)
 
 
